45c38d090e4c6d3f98fab31042aaeb250afc937b
Bamboo Data <developers@bamboo-data.co.uk>
2019-03-02 12:10:10 +0000

    Replace XML service definition with YAML

--- DependencyInjection/InoriTwitterAppExtension.php
+++ DependencyInjection/InoriTwitterAppExtension.php
@@ -4,14 +4,14 @@ namespace Inori\TwitterAppBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 
 class InoriTwitterAppExtension extends Extension
 {
     protected $resources = array(
-        'twitter_app' => 'twitter_app.xml',
+        'twitter_app' => 'twitter_app.yml',
     );
 
     public function load(array $configs, ContainerBuilder $container)
@@ -21,10 +21,10 @@ class InoriTwitterAppExtension extends Extension
         $config = $processor->processConfiguration($configuration, $configs);
 
         $this->loadDefaults($container);
-        
+
         if (isset($config['alias'])) {
             $container->setAlias($config['alias'], 'inori_twitter_app');
-        }        
+        }
 
         foreach (array('file', 'consumer_key', 'consumer_secret', 'oauth_token', 'oauth_token_secret') as $attribute) {
             if (isset($config[$attribute])) {
@@ -32,10 +32,10 @@ class InoriTwitterAppExtension extends Extension
             }
         }
     }
-    
+
     protected function loadDefaults($container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(array(__DIR__.'/../Resources/config', __DIR__.'/Resources/config')));
+        $loader = new YamlFileLoader($container, new FileLocator(array(__DIR__.'/../Resources/config', __DIR__.'/Resources/config')));
 
         foreach ($this->resources as $resource) {
             $loader->load($resource);
